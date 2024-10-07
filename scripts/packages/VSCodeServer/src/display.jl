e99212259b9f8092fe66feeab5963730394c69b9
xgdgsc <xgdgsc@gmail.com>
2024-10-07 10:41:19 +0800

    up

--- scripts/packages/VSCodeServer/src/display.jl
+++ scripts/packages/VSCodeServer/src/display.jl
@@ -81,19 +81,6 @@ function persistPlot(d::InlineDisplay, mime::String, payload::String)
     end
 end
 
-function persistPlot(d::InlineDisplay, mime::String, payload::String)
-    persistOutputFilePath = d.filename * ".vsjlplt"
-    jsonD = Dict()
-    if isfile(persistOutputFilePath)
-        jsonD = JSON.parsefile(persistOutputFilePath)
-    end
-    jsonD[string(d.endLine)] = Dict("startLine" => d.startLine, "startCol" => d.startColumn, "endLine" => d.endLine, "endCol" => d.endColumn,
-        "mime" => mime, "payload" => payload, "code_hash" => d.codeHash)
-    open(persistOutputFilePath, "w") do io
-        JSON.print(io, jsonD)
-    end
-end
-
 function Base.display(d::InlineDisplay, m::MIME, @nospecialize(x))
     if !PLOT_PANE_ENABLED[]
         with_no_default_display(() -> display(m, x))
